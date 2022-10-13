58d6a1b69c24d76e7f28930a0cf8efabd2c278b3
Jeysh-git <jeyshu11@gmail.com>
2022-10-13 11:38:59 +0530

    task3ab changes

--- app/components/user-cards.js
+++ app/components/user-cards.js
@@ -9,45 +9,49 @@ export default Component.extend({
     teams: computed ('members.@each.team',function(){
 
         return this.members.mapBy('team').uniq()
-        
-        // console.log("hi",arr1)
-        // let output =  arr1.filter((item,
-        //     index) => arr1.indexOf(item) === index);
-        
-        // // })
-        // // return output;
-        // console.log("hello",output)
+      
+    }),
+
+    fnamesort: computed ('members.@each.first_name',function(){
+        return this.get('members').sortBy('first_name')
+    }),
+
+    fnamesortDesc: computed ('members.@each.first_name',function(){
+        return this.get('members').sortBy('first_name').reverse()
+    }), 
+
+    lnamesort: computed ('members.@each.last_name',function(){
+        return this.get('members').sortBy('last_name')
+    }),
+
+    lnamesortDesc: computed ('members.@each.last_name',function(){
+        return this.get('members').sortBy('last_name').reverse()
+    }),
+
+    datesort: computed ('members.@each.joiningDate',function(){
+        return this.get('members').sortBy('joiningDate')
     }),
 
     // selectedOption: null,
-    sortProperties: '[first_name:asc]',
-    sortedSongs: computed.sort('members', this.sortProperties),
+   
     actions: {
         
         getTeams(teamName) {
            return this.set('teamsl',this.get('members').filterBy('team',teamName) )
             // return this.teamsl;
-        }
-    ,
-        showAllTeams(){
-            // console.log("helloooo")
-            return this.set('teamsl',this.get('members').sortBy(function (a, b) {
-                if (a.first_name < b.first_name) {
-                  return -1;
-                }
-                if (a.first_name > b.first_name) {
-                  return 1;
-                }
-                return 0;
-                console.log(users);  
-              })
-              )
-    },
+        }  ,
 
-    sortBy(sortProperties) {
-              this.set('sortProperties', [sortProperties]);
-              
-            },    
+        sortByFirstname(){
+            return this.set('teamsl',this.fnamesort)
+        },
+
+        sortByLastname(){
+            return this.set('teamsl',this.lnamesort)
+        },
+
+        sortByDate() {
+            return this.set('teamsl',this.datesort)
+        }
     
 }
 
