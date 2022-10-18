4c4dc275f1dd1472ab339d64e4440702c2e85f04
Jeysh-git <jeyshu11@gmail.com>
2022-10-18 16:21:08 +0530

    task3ab changes

--- app/components/user-cards.js
+++ app/components/user-cards.js
@@ -2,60 +2,24 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+
     select_team:"All Employees",
+
     teamsl : computed ('members.@each.team',function(){
         return this.get('members').filterBy('team')
     }) ,
 
     teams: computed ('members.@each.team',function(){
-
         return this.members.mapBy('team').uniq()
-      
     }),
 
-    fnamesort: computed ('members.@each.first_name',function(){
-        return this.get('members').sortBy('first_name')
-    }),
-
-    fnamesortDesc: computed ('members.@each.first_name',function(){
-        return this.get('members').sortBy('first_name').reverse()
-    }), 
-
-    lnamesort: computed ('members.@each.last_name',function(){
-        return this.get('members').sortBy('last_name')
-    }),
-
-    lnamesortDesc: computed ('members.@each.last_name',function(){
-        return this.get('members').sortBy('last_name').reverse()
-    }),
-
-    datesort: computed ('members.@each.joiningDate',function(){
-        return this.get('members').sortBy('joiningDate')
-    }),
-
-    // selectedOption: null,
-   
     actions: {
         
         getTeams(teamName) {
             this.set("select_team",teamName);
-           return this.set('teamsl',this.get('members').filterBy('team',teamName) )
-            // return this.teamsl;
+            return this.set('teamsl',this.get('members').filterBy('team',teamName) )
         }  ,
-
-        sortByFirstname(){
-            return this.set('teamsl',this.fnamesort)
-        },
-
-        sortByLastname(){
-            return this.set('teamsl',this.lnamesort)
-        },
-
-        sortByDate() {
-            return this.set('teamsl',this.datesort)
-        }
-    
-}
+    }
 
 });
      
