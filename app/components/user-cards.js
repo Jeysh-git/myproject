1a7395aa8b2ceabae35302d5d86d531d6defe085
Jeysh-git <jeyshu11@gmail.com>
2022-10-26 20:17:29 +0530

    1

--- app/components/user-cards.js
+++ app/components/user-cards.js
@@ -2,32 +2,32 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-    
-    select_team:"All Employees",
 
-    teamsl : computed ('members.@each.team',function(){
-        return this.get('members').filterBy('team')
+    select_team:"All Employees",
+    team:null,
+
+    teamsl : computed ('team',function(){
+        return this.get('team')
     }) ,
 
     teams: computed ('members.@each.team',function(){
-
         return this.members.mapBy('team').uniq()
-      
     }),
-   
+
+    init() {
+        this._super(...arguments);
+        this.set('team',this.get('members').filterBy('team') );
+      },
+
     actions: {
         
         getTeams(teamName) {
+            
             this.set("select_team",teamName);
-           return this.set('teamsl',this.get('members').filterBy('team',teamName) )
-            // return this.teamsl;
-        }  ,
+            this.set('team',this.get('members').filterBy('team',teamName) )
 
-        sortByFields(name){
-            return this.set('teamsl',this.get('members').sortBy(name))
-        },
-    
-}
+        }  ,
+    }
 
 });
      
