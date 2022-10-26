76a463ffbc31501cb483e1fef28b363c6563867d
Jeysh-git <jeyshu11@gmail.com>
2022-10-26 20:38:30 +0530

    updated code

--- app/components/user-cards.js
+++ app/components/user-cards.js
@@ -3,21 +3,29 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
 
-    select_team:"All Employees",
+    selected_option:"All Employees",
+    team:null,
 
-    teamsl : computed ('members.@each.team',function(){
-        return this.get('members').filterBy('team')
+    teamslist : computed ('team',function(){
+        return this.get('team')
     }) ,
 
     teams: computed ('members.@each.team',function(){
         return this.members.mapBy('team').uniq()
     }),
 
+    init() {
+        this._super(...arguments);
+        this.set('team',this.get('members').filterBy('team') );
+      },
+
     actions: {
         
         getTeams(teamName) {
-            this.set("select_team",teamName);
-            return this.set('teamsl',this.get('members').filterBy('team',teamName) )
+            
+            this.set("selected_option",teamName);
+            this.set('team',this.get('members').filterBy('team',teamName) )
+
         }  ,
     }
 
