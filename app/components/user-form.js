c026082fa73266f15638fe3de56ec399ed744237
Jeysh-git <jeyshu11@gmail.com>
2022-11-13 20:37:20 +0530

    updated slide-modal

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
+import EmberObject from '@ember/object';
 
 export default Component.extend({
 
-    title:'Add Employee'
+    title:'Add Employee',
+    formbuttons:null,
+
+    init(){
+        this._super(...arguments);
+        this.set('formbuttons', [
+            EmberObject.create({ classname: 'apply-btn',title:'Apply' }),
+            EmberObject.create({ classname: 'reset-btn',title:'Reset' }),
+            
+          ]);
+    }
 });
