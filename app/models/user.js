ee80410af41af71875f8c1ca4cf6f3e8fb532c8e
Jeysh-git <jeyshu11@gmail.com>
2022-11-18 20:58:40 +0530

    change

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,71 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import { validator,buildValidation } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const models = [
+    {
+      modelAttr: "first_name",
+      presence: true,
+      regexp: /^[a-zA-Z]+$/,
+      formatErrorMessage: 'First name field should have alphabets only',
+      minLength: 2,
+      maxLength:128,
+      lengthErrorMessage: 'First name should have atleast two characters'
+    },
+
+    {
+        modelAttr: "last_name",
+        presence: true,
+        regexp: /^[a-zA-Z]+$/,
+        formatErrorMessage: 'Last name field should have alphabets only',
+        minLength: 2,
+        maxLength:128,
+        lengthErrorMessage: 'Last name should have atleast two characters'
+      },
+
+    {
+      modelAttr: "email",
+      presence: true,
+      regexp: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      formatErrorMessage: 'The email is not valid format'
+    },
+
+    {
+        modelAttr: "designation",
+        presence: true,
+        regexp: /^[a-zA-Z]+$/,
+        formatErrorMessage: 'The name field should have alphabets only',
+        minLength: 2,
+        maxLength:128,
+        lengthErrorMessage: 'Designation should have atleast two characters'
+      },
+    
+  ]
+  
+  const buildValidations = {};
+  // iterating the models json and set each validator for a model
+  models.forEach(model => {
+    buildValidation[model.modelAttr] = {
+      validators: [
+        validator('presence', {
+          presence: model.presence,
+          message: `${model.modelAttr} is required`
+        }),
+        validator('format',{
+          regex: model.regexp,
+          message: model.formatErrorMessage
+        }),
+        validator('length', {
+          min: model.minLength,
+          max: model.maxLength,
+          message: model.lengthErrorMessage
+        })
+      ]
+    };
+  });
+
+export default DS.Model.extend(Validations,{
+
     first_name: DS.attr('string'),
     last_name: DS.attr('string'),
     email: DS.attr('string'),
